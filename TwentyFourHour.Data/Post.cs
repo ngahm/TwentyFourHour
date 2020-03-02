cdf184dfb0dd28597913f543b14fd43f5587a77a
Jeremy Hansen <jhansen1344@gmail.com>
2020-03-02 16:59:47 -0500

    foreign keys added

--- TwentyFourHour.Data/Post.cs
+++ TwentyFourHour.Data/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace TwentyFourHour.Data
         public string ID { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
-        public User Author { get; set; }
+        [ForeignKey(nameof(Author))]
+        public int AuthorID { get; set; }
+        public virtual User Author { get; set; }
     }
 }
