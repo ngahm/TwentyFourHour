33c7e96533fc435eb59a633e71ed0704e06fe375
Jeremy Hansen <jhansen1344@gmail.com>
2020-03-02 20:37:14 -0500

    reply and like scaffolded out

---
+++ TwentyFourHour.Services/LikeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyFourHour.Services
+{
+    public class LikeService
+    {
+        //POST
+
+        //GET ALL
+
+        //GET BY ID
+
+    }
+}
